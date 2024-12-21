e8bbed032c4e1319b331f8d571dd729d85a3233c
Yash Gaurkar <yashgaurkar0@gmail.com>
2024-12-21 19:47:20 +0530

    add links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,24 @@ export function Navbar() {
 
 				{/* PORTFOLIO TITLE */}
 				<div className={"flex flex-shrink-0 items-center"}>
-					<span className={"md:text-5xl text-3xl font-Nothing font-bold hover:text-white cursor-pointer"}>Yash Gaurkar</span>
+					<span
+						className={"md:text-5xl text-3xl font-Nothing font-bold hover:text-white cursor-pointer"}>Yash Gaurkar</span>
 				</div>
 
 				{/*	PORTFOLIO SOCIAL ICONS */}
 				<div className={"flex items-center justify-center gap-4 text-2xl"}>
-					<FaLinkedinIn className={"cursor-pointer hover:text-white"}/>
-					<FaGithub className={"cursor-pointer hover:text-white"}/>
-					<FaFacebook className={"cursor-pointer hover:text-white"}/>
-					<FaHackerrank className={"cursor-pointer hover:text-white"}/>
+					<a href="https://www.linkedin.com/in/yash-gaurkar-a897b3228/" target="_blank" rel="noreferrer">
+						<FaLinkedinIn className={"cursor-pointer hover:text-white"}/>
+					</a>
+					<a href="https://github.com/yashG0" target="_blank" rel="noreferrer">
+						<FaGithub className={"cursor-pointer hover:text-white"}/>
+					</a>
+					<a href="#">
+						<FaFacebook className={"cursor-pointer hover:text-white"}/>
+					</a>
+					<a href="https://www.hackerrank.com/profile/yashgaurkar0" target="_blank" rel="noreferrer">
+						<FaHackerrank className={"cursor-pointer hover:text-white"}/>
+					</a>
 				</div>
 			</nav>
 		</>
